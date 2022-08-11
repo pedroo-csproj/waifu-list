b3c23fa6acf86e1c84c8d4a77ddc55e5a23f7455
pedroo-csproj <pedrooctavio.dev@outlook.com>
2022-08-11 02:05:17 -0300

    server configuration improved

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,28 @@
+import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+dotenv.config()
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(helmet.hidePoweredBy())
+
+if (process.env.ENVIRONMENT === 'development') {
+  app.use(morgan(':method :url :status - :response-time ms'))
+}
 
 app.use('', (req, res) => {
   return res.status(201).json({ yamete: 'kudasai oni-chan' })
 })
 
-app.listen(3000, () => {
-  console.log('app running')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.info(`api running on port ${port}`)
 })
